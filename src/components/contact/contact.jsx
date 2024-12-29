d2fc374fbfa0f0c52071aa56c9e71900fdc7770c
Birk Dinkelacker <birk.dinkelacker2009@gmail.com>
2024-12-29 10:40:58 +0100

    Update contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 
 const Contact = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
 
+    const formData = new FormData();
     formData.append("access_key", "237e4fc6-d958-4aa0-8403-6bee26badc36");
+    formData.append("name", formValues.name);
+    formData.append("email", formValues.email);
+    formData.append("message", formValues.message);
 
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
@@ -22,19 +36,35 @@ const Contact = () => {
 
     if (res.success) {
       console.log("Success", res);
+      setFormValues({ name: "", email: "", message: "" }); // Eingabefelder zurücksetzen
     }
   };
+
   return (
     <div className="contact">
       <form onSubmit={onSubmit}>
         <h1>Contact</h1>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input id="name" type="text" name="name" required />
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={formValues.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input id="email" type="email" name="email" required />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message:</label>
@@ -42,6 +72,8 @@ const Contact = () => {
             id="message"
             rows={4}
             name="message"
+            value={formValues.message}
+            onChange={handleChange}
             placeholder="Type text here..."
             required
           ></textarea>
