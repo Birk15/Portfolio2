4ef7c6897ddb82d0a5556895176f99e185b18571
Birk Dinkelacker <birk.dinkelacker2009@gmail.com>
2024-12-30 10:15:06 +0100

    Update offers.jsx

--- src/components/offers/offers.jsx
+++ src/components/offers/offers.jsx
@@ -68,7 +68,7 @@ const Offers = () => {
       <Row>
         {offers.map((offer) => (
           <Col key={offer.id}>
-            <Card style={{ marginBottom: 50, minWidth: 300, minHeight: 240 }}>
+            <Card style={{ marginBottom: 50, minWidth: 300, height: 260 }}>
               <Card.Img
                 className="img"
                 variant="top"
